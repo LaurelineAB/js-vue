331061dc5ee6f0c543a741c206d34e3168f5e379
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-22 11:40:27 +0200

    Fin de l'exercice sur les formulaires

--- forms/assets/js/apps/app2.js
+++ forms/assets/js/apps/app2.js
@@ -7,7 +7,7 @@ export default {
     methods : {
         addTask (event) {
             let newTask = event.target.elements.task.value;
-            this.tasks.push(newTask);
+            this.tasks.push({toDo: newTask, done: false});
         }
     }
 }
